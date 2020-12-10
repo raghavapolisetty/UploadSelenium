1483dccd6a2d647a02aa171f540c09ef703cf9b1
Admin <Admin@Madala>
2020-12-10 19:30:02 +0530

    third commit

--- Testing/src/Firstclass.java
+++ Testing/src/Firstclass.java
@@ -1,9 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Firstclass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
+		
+		static WebDriver driver;
+	
+		@Test
+		public void Launchbrowser()
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\seleniumsession\\drivers\\chromedriver.exe");
+			
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.flipkart.com");
+			
+			System.out.println("opened");
+			System.out.println(driver.getTitle());
+		}
 
 }
